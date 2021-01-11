b6742d8e994459b5169ba35152f06b9cfe6fb4ac
Wayyjan <wjianyap@gmail.com>
2021-01-11 16:53:37 +0800

    Update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\NYP\Y1S2\App Development Project\Assignment\Codes\Unified-Help_Combined\venv
+set VIRTUAL_ENV=E:\App Dev\Unified-Help_Combined\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
